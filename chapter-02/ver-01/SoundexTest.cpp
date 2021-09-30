a2cee20a9fce279646d2e21b24c37d61bb502584
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-09-30 19:55:37 +0200

    Add a Soundex class so example compiles.

--- chapter-02/ver-01/SoundexTest.cpp
+++ chapter-02/ver-01/SoundexTest.cpp
@@ -1,5 +1,8 @@
-#include "gmock/gmock.h"    
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { 
-   Soundex soundex;   
-}
+class Soundex {
+};
 
+#include "gmock/gmock.h"
+
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+   Soundex soundex;
+}
