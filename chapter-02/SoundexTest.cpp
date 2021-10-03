042697fabf96e311452b1f51a7c422e079f76c9e
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-10-03 16:16:40 +0200

    Reduce test to a single row.

--- chapter-02/SoundexTest.cpp
+++ chapter-02/SoundexTest.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include "gmock/gmock.h"
 
-using ::testing::Eq;
+using namespace testing;
 
 class Soundex {
 public:
@@ -20,11 +20,9 @@ public:
 };
 
 TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-   auto encoded = soundex.encode("A");
-   ASSERT_THAT(encoded, Eq("A000"));
+   ASSERT_THAT(soundex.encode("A"), Eq("A000"));
 }
 
 TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
-	auto encoded = soundex.encode("I");
-	ASSERT_THAT(encoded, Eq("I000"));
+	ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
