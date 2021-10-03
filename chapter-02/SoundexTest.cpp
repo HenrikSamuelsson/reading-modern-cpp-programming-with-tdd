afc7c07f6a4318230f1f1839fd489bfb42f6f9d2
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-10-03 16:09:14 +0200

    Add a text fixture to get rid of duplication.

--- chapter-02/SoundexTest.cpp
+++ chapter-02/SoundexTest.cpp
@@ -8,27 +8,23 @@ public:
    std::string encode(const std::string& word) const {
       return zeroPad(word);
    }
-   
 private:
 	std::string zeroPad(const std::string& word) const {
 		return word + "000";
 	}
-   
 };
 
+class SoundexEncoding: public testing::Test {
+public:
+		Soundex soundex;
+};
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-   Soundex soundex;
-
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
    auto encoded = soundex.encode("A");
-
    ASSERT_THAT(encoded, Eq("A000"));
 }
 
-TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
-	Soundex soundex;
-	
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
 	auto encoded = soundex.encode("I");
-	
 	ASSERT_THAT(encoded, Eq("I000"));
 }
