d6f03e11e85520908dd5f6e81b0c15d301835d4b
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-10-07 19:57:15 +0200

    Makes the latest test pass.

--- chapter-02/soundex.h
+++ chapter-02/soundex.h
@@ -8,29 +8,40 @@ class Soundex
 public:
    static const size_t MaxCodeLength{4};
 
-   std::string encode(const std::string& word) const {
+   std::string encode(const std::string& word) const 
+   {
       return zeroPad(head(word) + encodedDigits(word));      
    }
 
 private:
-   std::string head(const std::string& word) const {
+   std::string head(const std::string& word) const 
+   {
       return word.substr(0, 1);
    }
 
-   std::string encodedDigits(const std::string& word) const{
-      if (word.length() > 1) {
-         return encodeDigit();
+   std::string encodedDigits(const std::string& word) const
+   {
+      if (word.length() > 1) 
+      {
+         return encodeDigit(word[1]);
       }
-      else {
+      else 
+      {
          return "";
       }
    }
 
-   std::string encodeDigit() const {
+   std::string encodeDigit(char letter) const 
+   {
+      if (letter == 'c') 
+      {
+         return "2";
+      }
       return "1";
    }
 
-   std::string zeroPad(const std::string& word) const {
+   std::string zeroPad(const std::string& word) const 
+   {
       auto zerosNeeded = MaxCodeLength - word.length();
       return word + std::string(zerosNeeded, '0');
    }
