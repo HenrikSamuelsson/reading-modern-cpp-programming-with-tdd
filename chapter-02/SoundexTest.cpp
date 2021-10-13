6bc984f03d39870f1eeae00d06b4ab7c981f896b
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-10-13 20:20:08 +0200

    Add test that checks length.

--- chapter-02/SoundexTest.cpp
+++ chapter-02/SoundexTest.cpp
@@ -27,3 +27,7 @@ TEST_F(SoundexEncoding, IgnoresAplphabetics) {
 TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
 	ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
 }
+
+TEST_F(SoundexEncoding, LimitLengthToFourCharacters) {
+	ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
