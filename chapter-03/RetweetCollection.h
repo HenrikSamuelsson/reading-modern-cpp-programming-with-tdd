7c4e951ad2c518ff09de439d691b21ff5747d4d1
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-11-01 19:15:35 +0100

    Add missing header file.

---
+++ chapter-03/RetweetCollection.h
@@ -0,0 +1,18 @@
+
+#ifndef CHAPTER_03_RETWEETCOLLECTION_H_
+#define CHAPTER_03_RETWEETCOLLECTION_H_
+
+class RetweetCollection {
+public:
+    bool isEmpty() const
+    {
+       return 0 == size();
+    }
+
+    unsigned int size() const
+    {
+         return 0;
+    }
+};
+
+#endif // CHAPTER_03_RETWEETCOLLECTION_H_
